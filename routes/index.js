d18f6c448a9fee9eb90bcb021298ddeb8ada1dc3
Matthew Chu <Matthew Chu>
2016-12-27 00:09:34 -0500

    homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.redirect('/home');
+});
+
+router.get('/home', function(req, res, next) {
+  res.send('hi paul');
 });
 
 module.exports = router;
